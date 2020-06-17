85766b1cdb0fc601b0e2f7e05fd427575c8e500c
Tim Horie <tak@thorie.com>
2020-06-16 23:42:30 -0700

    Adding index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+?><html lang="en">
+<head>
+<script src="https://microlancer.io/media/js/libs/preact-8.4.2.dev.js"></script>
+<script src="https://microlancer.io/media/js/libs/preact-router-2.6.1.min.js"></script>
+</head>
+<body id='main'>
+</body>
+</html>
